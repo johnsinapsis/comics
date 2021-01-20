3bcc6e1e8bdb67f48c11a30431620b796acaa0bd
john jairo gonzalez <johnsinapsis@gmail.com>
2021-01-20 09:34:15 -0500

    Pruebas iniciales de modularización

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faTh, faThList } from '@fortawesome/free-solid-svg-icons'
+
+import Hola from './views/ComicList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path='/' component={Hola}></Route>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/ComicList/ComicList.js
@@ -0,0 +1,13 @@
+//import React from 'react'
+import styles from './ComicList.module.css'
+
+const Hola = ()=>{
+    return(
+      <div>
+        <h3 className={styles.base}>Hola Mundo!!! Sigo aquí Funcionando</h3>
+      </div>
+    )
+  
+}
+
+export default Hola;
